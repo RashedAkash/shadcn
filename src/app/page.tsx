f8473aae4be08de21c5cc99f460c6d7f115df075
hablu <131282586+hablu@users.noreply.github.com>
2024-01-17 12:54:29 +0600

    card added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,33 @@
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
 
 export default function Home() {
   return (
     <main className=' border-2'>
       <Button variant={'destructive'}>button</Button>
+      <Card className=' border-2 w-1/2'>
+  <CardHeader>
+    <CardTitle>Card Title</CardTitle>
+    <CardDescription>Card Description</CardDescription>
+  </CardHeader>
+  <CardContent>
+    <p>Card Content</p>
+  </CardContent>
+  <CardFooter>
+    <p>Card Footer</p>
+  </CardFooter>
+</Card>
+
+
   </main>
   )
 }
